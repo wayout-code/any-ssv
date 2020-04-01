8d2cb1d5b69a9c0e87d4e5dc69245addb9407543
Jiri Kostiha <ijkdata@gmail.com>
2020-04-01 13:51:45 +0200

    feat: parse lines as dynamic objects

---
+++ src/AnySsv/DynamicObject.Parser.cs
@@ -0,0 +1,58 @@
+﻿namespace Wayout.AnySsv
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+    using System.Linq;
+
+    /// <summary>
+    /// Ssv format to dynamic object parser.
+    /// </summary>
+    public class DynamicObjectParser
+    {
+        public DynamicObjectParser()
+        {
+        }
+
+        private SsvParser _ssvParser = new SsvParser();
+
+        public IEnumerable<ExpandoObject> ParseData(string format)
+        {
+            var ssv = _ssvParser.Parse(format);
+            return Parse(ssv);
+        }
+
+        public IEnumerable<ExpandoObject> Parse(Ssv format)
+        {
+            var tableNames = format.GetTablesNames().ToArray();
+
+            foreach (var line in format.GetTableData(0))
+            {
+                IDictionary<string, object> obj = new ExpandoObject();
+
+                var columnNamesLine = format.GetTableColumnNamesLine();
+                var columnNames = columnNamesLine.Values.Select(c => c.Trim()).ToArray();
+
+                for (int i = 0; i < line.Values.Count; i++)
+                {
+                    var v = line.Values[i].Trim();
+                    if (v.StartsWith("\"") && v.EndsWith("\""))
+                    {
+                        obj[columnNames[i]] = v;
+                    }
+                    else if (int.TryParse(v, out int result))
+                    {
+                        obj[columnNames[i]] = result;
+                    }
+                    else if (double.TryParse(v, out double dresult))
+                    {
+                        obj[columnNames[i]] = dresult;
+                    }
+
+                    obj[columnNames[i]] = v;
+                }
+
+                yield return (ExpandoObject) obj;
+            }
+        }
+    }
+}
---
+++ src/Quality/Quality/DynamicObject.Parser.Test.cs
@@ -0,0 +1,39 @@
+﻿namespace Wayout.AnySsv.Quality
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicObjectParserTest
+    {
+        [TestMethod]
+        public void ParseSigleLineAsObject()
+        {
+            var parser = new DynamicObjectParser();
+            var ssv = SsvFactory.Create3ColumnsNDataLines(1);
+            
+            dynamic dynObj = parser.Parse(ssv).First();
+
+            Assert.AreEqual("a1", dynObj.A);
+            Assert.AreEqual("b1", dynObj.B);
+            Assert.AreEqual("c1", dynObj.C);
+        }
+
+        [TestMethod]
+        public void Parse3LineAsObjects()
+        {
+            var parser = new DynamicObjectParser();
+            var ssv = SsvFactory.Create3ColumnsNDataLines(3, "T");
+
+            var dynObjects = parser.Parse(ssv).ToArray();
+
+            Assert.AreEqual(3, dynObjects.Count());
+            dynamic dynObj3 = dynObjects[2];
+            Assert.AreEqual("a3", dynObj3.A);
+            Assert.AreEqual("b3", dynObj3.B);
+            Assert.AreEqual("c3", dynObj3.C);
+        }
+    }
+}
